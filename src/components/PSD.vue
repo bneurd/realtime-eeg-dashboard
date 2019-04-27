32c0c79994e1be1f51a2d9a29bc5923c14120687
igornfaustino <igornfaustino@gmail.com>
2019-04-27 20:58:40 -0300

    pass data to PSD component

--- src/components/PSD.vue
+++ src/components/PSD.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="channels"></div>
+  <div>
+    <h1>PSD</h1>
+  </div>
 </template>
 
 <style>
@@ -16,80 +18,19 @@ canvas {
 </style>
 
 <script>
-import { getChartSmoothieDefaults } from "@/utils/ChartOptions.js";
-import { COLORS } from "@/utils/Colors";
-
-var $ = require("jquery");
-
 export default {
-  name: "TimeSeries",
+  name: "PSD",
   data() {
-    return {
-      charts: [],
-      series: [],
-      dataToEmitTemplate: [],
-      dataToEmit: [],
-      firtsPkg: true,
-      channels: []
-    };
+    return {};
   },
-  sockets: {
-    eeg(dataStr) {
-      let data = JSON.parse(dataStr);
-      if (this.firtsPkg) {
-        console.log(data);
-        this.channels = data.channels;
-        this.firtsPkg = false;
-        this.initGraph();
-      }
-      this.series.forEach((serie, idx) => {
-        serie.append(Date.now(), data.eeg[idx]);
-        this.dataToEmit[idx].push(data.eeg[idx]);
-      });
-      this.$emit("frequency", data.fs);
-      if (this.dataToEmit[0].length === data.fs) {
-        this.$emit("dataForOneSec", this.dataToEmit);
-        this.dataToEmit = this.dataToEmit.map(_data => [])
-      }
-    }
+  props: {
+    dataForOneSec: Array,
   },
-  methods: {
-    initGraph() {
-      // console.log(this.channels)
-      this.channels.forEach((channel, idx) => {
-        // Add chart element
-        $("#channels").append(`
-        <div class="row" style="width:100%;">
-          <div class="col-1 channel-name">
-            ${channel}
-          </div>
-          <div class="col-11">
-            <canvas id="${channel}" style="width:100%; height:90px"></canvas>
-          </div>
-        </div>`);
-
-        // get chart element
-        const canvas = document.getElementById(channel);
-
-        // Create chart
-        this.charts.push(new SmoothieChart(getChartSmoothieDefaults()));
-
-        // create time serie
-        this.series.push(new TimeSeries());
-
-        // add time serie
-        this.charts[idx].addTimeSeries(this.series[idx], {
-          strokeStyle: COLORS[idx]
-        });
-
-        // createDataToEmitTemplate
-        this.dataToEmitTemplate.push([])
-
-        // render chart
-        this.charts[idx].streamTo(canvas);
-      });
-      this.dataToEmit = [...this.dataToEmitTemplate]
-    }
-  }
+  watch: {
+      dataForOneSec: (newValue) => {
+          console.log(newValue.length)
+      }
+  },
+  methods: {}
 };
 </script>

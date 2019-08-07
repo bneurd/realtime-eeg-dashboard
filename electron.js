83a32cbe944aa866f2488de6b10d19063355db78
igornfaustino <igornfaustino@gmail.com>
2019-08-07 20:22:16 -0300

    refactory electron config

---
+++ electron.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron')
+
+let win
+
+function createWindow() {
+	win = new BrowserWindow({ width: 800, height: 600 })
+	win.maximize()
+
+	if (process.env.NODE_ENV === 'DEV') {
+		win.webContents.openDevTools()
+		win.loadURL('http://localhost:8080/')
+	} else {
+		win.loadURL(`file://${process.cwd()}/dist/index.html`)
+	}
+
+
+	win.on('closed', () => {
+		win = null
+	})
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
+})
+
+app.on('activate', () => {
+	if (win === null) {
+		createWindow()
+	}
+})
